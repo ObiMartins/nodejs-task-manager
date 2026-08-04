89e3bbec81498ebfb62f37194ce9416c276d390a
Obi Martins <73021405+ObiMartins@users.noreply.github.com>
2026-08-04 05:44:03 +0000

    Add health check endpoint

--- src/app.js
+++ src/app.js
@@ -1,15 +1,18 @@
 const express = require("express");
 
+const healthRoutes = require("./routes/healthRoutes");
+
 const app = express();
 
-// Middleware
 app.use(express.json());
 
-// Root route
 app.get("/", (req, res) => {
-  res.status(200).json({
+  res.json({
     message: "Welcome to the Node.js Task Manager API"
   });
 });
+
+// Health endpoint
+app.use("/health", healthRoutes);
 
 module.exports = app;
